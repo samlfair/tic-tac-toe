7906b1547777c51795ec2332fcb2dac5643224ce
Samuel Littlefair-Wallace <sam@littlefairwallace.ca>
2020-07-21 14:01:57 +0200

    Updates

---
+++ precache-manifest.c943eceb688b1d636fb7d54e5af5b679.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7dc903e56070d1228df983a0ee75ec49",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "7215dc1e4c19583202db",
+    "url": "/tic-tac-toe/static/js/2.92e59f10.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tic-tac-toe/static/js/2.92e59f10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd8ee56404afc25c5305",
+    "url": "/tic-tac-toe/static/js/main.7aff7501.chunk.js"
+  },
+  {
+    "revision": "7f32745e7b69030faee7",
+    "url": "/tic-tac-toe/static/js/runtime-main.df28867d.js"
+  }
+]);
